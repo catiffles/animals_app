9063f2a37523519c8bc89c568a74933742351b45
Susan <susan@marpy.net>
2015-03-05 12:33:51 -0500

    added seeds to classifications

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+mammals = Classifications.create(name: 'Mammals', vertebrate?: true, warm_blooded?: true)
+fish = Classifications.create(name: 'Fish', vertebrate?: true, warm_blooded?: false)
+birds = Classifications.create(name: 'Birds', vertebrate?: true, warm_blooded?: true)
+amphibians = Classifications.create(name: 'Amphibians', vertebrate?: true, warm_blooded?: false)
+reptiles = Classifications.create(name: 'Reptiles', vertebrate?: true, warm_blooded?: false)
+insects = Classifications.create(name: 'Mammals', vertebrate?: false, warm_blooded?: false)
