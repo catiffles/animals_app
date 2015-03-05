bcfac6a2313db1b261a05f8df4133cefb222c208
Caitlyn Landry <caitlyn.landry@gmail.com>
2015-03-05 12:07:39 -0500

    Add Classification model

---
+++ db/migrate/20150305170634_create_classifications.rb
@@ -0,0 +1,11 @@
+class CreateClassifications < ActiveRecord::Migration
+  def change
+    create_table :classifications do |t|
+      t.string :name
+      t.boolean :vertebrate?
+      t.boolean :warm-blooded?
+
+      t.timestamps null: false
+    end
+  end
+end
---
+++ spec/factories/classifications.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :classification do
+    name "MyString"
+vertebrate? false
+warm-blooded? false
+  end
+
+end
